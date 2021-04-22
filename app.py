4861080cde1a9fd707d94d65177fa9909f01982e
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-04-21 18:11:46 -0700

    Working app.py home route, will build out

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 from flask_pymongo import PyMongo
+import json
+from bson import json_util
 
 app = Flask(__name__)
 
 mongo = PyMongo(app, uri="mongodb://localhost:27017/housing_data_db")
+
+@app.route('/')
+def home():
+
+    housing_info = list(mongo.db.housing_data_db.find({'data': 'state'}))
+
+    return json.dumps(housing_info, indent=4, default=json_util.default)
+
+if __name__ == "__main__":
+    app.run(debug=True)
