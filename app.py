fa3464d98a6cebf6d3de7779f189b558381059ed
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-04-21 18:44:51 -0700

    All routes are working. A little janky, will clean up later

--- app.py
+++ app.py
@@ -10,9 +10,33 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/housing_data_db")
 @app.route('/')
 def home():
 
-    housing_info = list(mongo.db.housing_data_db.find({'data': 'state'}))
+    return(
+        "Available Routes:<br>"
+        "/or => median rent data by state<br>"
+        "/us => median rent data nationally <br>"
+        "/<county> => full census dataset by county"
+        )
 
-    return json.dumps(housing_info, indent=4, default=json_util.default)
+
+@app.route("/<county>")
+def county_stats(county):
+    
+    county_info = list(mongo.db.housing_data_db.find({'county': county}))
+
+    return json.dumps(county_info, indent=4, default=json_util.default)
+
+@app.route("/or")
+def state_stats():
+
+    state_info = list(mongo.db.housing_data_db.find({'data': 'state'}))
+
+    return json.dumps(state_info, indent=4, default=json_util.default)
+
+@app.route("/us")
+def national_stats():
+    us_info = list(mongo.db.housing_data_db.find({'data': 'national'}))
+
+    return json.dumps(us_info, indent=4, default=json_util.default)
 
 if __name__ == "__main__":
     app.run(debug=True)
