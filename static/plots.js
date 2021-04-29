569bc4d55df3f36087e9aaa8edd175f4bdbdf0e4
Isabelle DeMartin <isabelledemartin.68@gmail.com>
2021-04-28 19:21:33 -0700

    pie chart commented out

--- static/plots.js
+++ static/plots.js
@@ -4,7 +4,7 @@ createPlots(countyName);
 
 function createPlots(countyName) {
 
-    d3.json(`/api/v1/or`).then(function (orData){
+    d3.json(`/api/v1/or`).then(function (orData) {
         console.log(orData)
 
         let orRent2015 = orData[0].state_median_rent_2015;
@@ -13,126 +13,130 @@ function createPlots(countyName) {
         let orRent2018 = orData[0].state_median_rent_2018;
         let orRent2019 = orData[0].state_median_rent_2019;
 
-    d3.json(`api/v1/us`).then(function (usData) {
-        console.log(usData);
+        d3.json(`api/v1/us`).then(function (usData) {
+            console.log(usData);
 
-        let usRent2015 = usData[0].us_median_rent_2015;
-        let usRent2016 = usData[0].us_median_rent_2016;
-        let usRent2017 = usData[0].us_median_rent_2017;
-        let usRent2018 = usData[0].us_median_rent_2018;
-        let usRent2019 = usData[0].us_median_rent_2019;
+            let usRent2015 = usData[0].us_median_rent_2015;
+            let usRent2016 = usData[0].us_median_rent_2016;
+            let usRent2017 = usData[0].us_median_rent_2017;
+            let usRent2018 = usData[0].us_median_rent_2018;
+            let usRent2019 = usData[0].us_median_rent_2019;
 
-    d3.json(`api/v1/${countyName}`).then(function (countyData) {
-        console.log(countyData);
+            d3.json(`api/v1/${countyName}`).then(function (countyData) {
+                console.log(countyData);
 
-        let countyFips = countyData[0].county_fips;
-        let countyName = countyData[0].county;
-        let medianRent2015 = countyData[0].county_median_rent_2015;
-        let medianRent2016 = countyData[0].county_median_rent_2016;
-        let medianRent2017 = countyData[0].county_median_rent_2017;
-        let medianRent2018 = countyData[0].county_median_rent_2018;
-        let medianRent2019 = countyData[0].county_median_rent_2019;
-        let medianIncome = countyData[0].county_median_income;
-        let renters = countyData[0].renters
-        let homeowners = countyData[0].homeowners
-
-
-        // 2019 Median Income 
-        d3.select("#income").html("");
-        d3.select("#income").append("h5").text("$" + medianIncome);
-        
-        // 2019 Percent of Income
-        let percentOfIncome = (medianRent2019 / (medianIncome / 12)) * 100
-        let percentOfIncomeFloat = parseFloat(percentOfIncome).toFixed(2);
-        d3.select("#rent").html("");
-        d3.select("#rent").append("h5").text(percentOfIncomeFloat + "%");
+                let countyFips = countyData[0].county_fips;
+                let countyName = countyData[0].county;
+                let medianRent2015 = countyData[0].county_median_rent_2015;
+                let medianRent2016 = countyData[0].county_median_rent_2016;
+                let medianRent2017 = countyData[0].county_median_rent_2017;
+                let medianRent2018 = countyData[0].county_median_rent_2018;
+                let medianRent2019 = countyData[0].county_median_rent_2019;
+                let medianIncome = countyData[0].county_median_income;
+                let renters = countyData[0].renters
+                let homeowners = countyData[0].homeowners
 
 
-        let trace1 = {
-            x: [2015, 2016, 2017, 2018, 2019],
-            y: [medianRent2015, medianRent2016, medianRent2017, medianRent2018, medianRent2019],
-            name: 'County Median Rent',
-            type: 'scatter'
-        };
+                // 2019 Median Income 
+                d3.select("#income").html("");
+                d3.select("#income").append("h5").text("$" + medianIncome);
 
-        let trace2 = {
-            x: [2015, 2016, 2017, 2018, 2019],
-            y: [usRent2015, usRent2016, usRent2017, usRent2018, usRent2019],
-            name: 'US Median Rent', 
-            type: 'scatter'
-        };
-
-        let trace3 = {
-            x: [2015, 2016, 2017, 2018, 2019],
-            y: [orRent2015, orRent2016, orRent2017, orRent2018, orRent2019],
-            name: 'Oregon Median Rent',
-            type: 'scatter'
-        };
+                // 2019 Percent of Income
+                let percentOfIncome = (medianRent2019 / (medianIncome / 12)) * 100
+                let percentOfIncomeFloat = parseFloat(percentOfIncome).toFixed(2);
+                d3.select("#rent").html("");
+                d3.select("#rent").append("h5").text(percentOfIncomeFloat + "%");
 
 
-        let data1 = [trace1, trace2, trace3];
+                let trace1 = {
+                    x: [2015, 2016, 2017, 2018, 2019],
+                    y: [medianRent2015, medianRent2016, medianRent2017, medianRent2018, medianRent2019],
+                    name: 'County Median Rent',
+                    type: 'scatter'
+                };
 
-        let layout = {
-            title: countyName,
-            xaxis: { title: "Year", tickformat: '.0f', dtick: 1 },
-            yaxis: { title: "Average Rent ($)" }
-        }
+                let trace2 = {
+                    x: [2015, 2016, 2017, 2018, 2019],
+                    y: [usRent2015, usRent2016, usRent2017, usRent2018, usRent2019],
+                    name: 'US Median Rent',
+                    type: 'scatter'
+                };
 
-        Plotly.newPlot('plot', data1, layout);
+                let trace3 = {
+                    x: [2015, 2016, 2017, 2018, 2019],
+                    y: [orRent2015, orRent2016, orRent2017, orRent2018, orRent2019],
+                    name: 'Oregon Median Rent',
+                    type: 'scatter'
+                };
 
 
+                let data1 = [trace1, trace2, trace3];
 
-        var width = 450;
-        var height = 450;
-        var margin = 40;
+                let layout = {
+                    title: countyName,
+                    xaxis: { title: "Year", tickformat: '.0f', dtick: 1 },
+                    yaxis: { title: "Average Rent ($)" }
+                }
 
-        var radius = Math.min(width, height) / 2 - margin;
-        d3.select("#pie").html("")
+                Plotly.newPlot('plot', data1, layout);
 
-        var svg = d3.select("#pie")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-        var plotData = { "Renters": renters, "Homeowners": homeowners };
+                // set the dimensions and margins for the graph
+                var width = 450;
+                var height = 450;
+                var margin = 40;
 
-        var pie = d3.pie()
-            .value(function (d) { return d.value; });
-        var data_ready = pie(d3.entries(plotData));
-        var color = d3.scaleOrdinal()
-            .domain(plotData)
-            .range(d3.schemeSet2);
+                //set the radius of the pieplot 
+                var radius = Math.min(width, height) / 2 - margin;
+                d3.select("#pie").html("")
 
-        svg
-            .selectAll('whatever')
-            .data(data_ready)
-            .enter()
-            .append('path')
-            .attr('d', d3.arc()
-                .innerRadius(0)
-                .outerRadius(radius)
-            )
-            .attr('fill', function (d) { return (color(d.data.key)) })
-            .attr("stroke", "black")
-            .style("stroke-width", "2px")
-            .style("opacity", 0.7);
-        var arcGenerator = d3.arc()
-            .innerRadius(0)
-            .outerRadius(radius);
+                //append the svg object to the div "#pie" 
+                var svg = d3.select("#pie")
+                    .append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .append("g")
+                    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-        svg
-            .selectAll('mySlices')
-            .data(data_ready)
-            .enter()
-            .append('text')
-            .text(function (d) { return d.data.key + " " + d.data.value })
-            .attr("transform", function (d) { return "translate(" + arcGenerator.centroid(d) + ")"; })
-            .style("text-anchor", "middle")
-            .style("font-size", 17);
-    })
-    })
+                var plotData = { "Renters": renters, "Homeowners": homeowners };
+
+                //compute positions on the chart
+                var pie = d3.pie()
+                    .value(function (d) { return d.value; });
+                var data_ready = pie(d3.entries(plotData));
+                var color = d3.scaleOrdinal()
+                    .domain(plotData)
+                    .range(d3.schemeSet2);
+
+                // build the pie chart 
+                svg
+                    .selectAll('')
+                    .data(data_ready)
+                    .enter()
+                    .append('path')
+                    .attr('d', d3.arc()
+                        .innerRadius(0)
+                        .outerRadius(radius)
+                    )
+                    .attr('fill', function (d) { return (color(d.data.key)) })
+                    .attr("stroke", "black")
+                    .style("stroke-width", "2px")
+                    .style("opacity", 0.7);
+                var arcGenerator = d3.arc()
+                    .innerRadius(0)
+                    .outerRadius(radius);
+
+                svg
+                    .selectAll('mySlices')
+                    .data(data_ready)
+                    .enter()
+                    .append('text')
+                    .text(function (d) { return d.data.key + " " + d.data.value })
+                    .attr("transform", function (d) { return "translate(" + arcGenerator.centroid(d) + ")"; })
+                    .style("text-anchor", "middle")
+                    .style("font-size", 17);
+            })
+        })
     })
 
 }
