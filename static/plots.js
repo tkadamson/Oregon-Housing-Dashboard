919dfe4b3f12de936cffdaf88ed5972b742d886a
araethomp <angela.r.thomp@gmail.com>
2021-04-28 19:21:24 -0700

    adding comments

--- static/plots.js
+++ static/plots.js
@@ -1,31 +1,39 @@
+// Set Baker County as initial load
 let countyName = "Baker"
 
+// Call on createPlots function
 createPlots(countyName);
+
 
 function createPlots(countyName) {
 
+    // Call in Oregon data from API
     d3.json(`/api/v1/or`).then(function (orData){
-        console.log(orData)
+        // console.log(orData)
 
+        // Assign Oregon data to variables
         let orRent2015 = orData[0].state_median_rent_2015;
         let orRent2016 = orData[0].state_median_rent_2016;
         let orRent2017 = orData[0].state_median_rent_2017;
         let orRent2018 = orData[0].state_median_rent_2018;
         let orRent2019 = orData[0].state_median_rent_2019;
-
+    
+    // Call in US data from API
     d3.json(`api/v1/us`).then(function (usData) {
-        console.log(usData);
-
+        // console.log(usData);
+        
+        // Assign US data to variables
         let usRent2015 = usData[0].us_median_rent_2015;
         let usRent2016 = usData[0].us_median_rent_2016;
         let usRent2017 = usData[0].us_median_rent_2017;
         let usRent2018 = usData[0].us_median_rent_2018;
         let usRent2019 = usData[0].us_median_rent_2019;
 
+    // Call in county data from API
     d3.json(`api/v1/${countyName}`).then(function (countyData) {
-        console.log(countyData);
+        // console.log(countyData);
 
-        let countyFips = countyData[0].county_fips;
+        // Assign county data to variables
         let countyName = countyData[0].county;
         let medianRent2015 = countyData[0].county_median_rent_2015;
         let medianRent2016 = countyData[0].county_median_rent_2016;
@@ -36,18 +44,17 @@ function createPlots(countyName) {
         let renters = countyData[0].renters
         let homeowners = countyData[0].homeowners
 
-
-        // 2019 Median Income 
+        // Grab income tag and assign 2019 median income 
         d3.select("#income").html("");
         d3.select("#income").append("h5").text("$" + medianIncome);
         
-        // 2019 Percent of Income
+        // Grab rent tag and assign 2019 percent of income
         let percentOfIncome = (medianRent2019 / (medianIncome / 12)) * 100
         let percentOfIncomeFloat = parseFloat(percentOfIncome).toFixed(2);
         d3.select("#rent").html("");
         d3.select("#rent").append("h5").text(percentOfIncomeFloat + "%");
 
-
+        // Create trace for county rental data
         let trace1 = {
             x: [2015, 2016, 2017, 2018, 2019],
             y: [medianRent2015, medianRent2016, medianRent2017, medianRent2018, medianRent2019],
@@ -55,6 +62,7 @@ function createPlots(countyName) {
             type: 'scatter'
         };
 
+        // Create trace for US rental data
         let trace2 = {
             x: [2015, 2016, 2017, 2018, 2019],
             y: [usRent2015, usRent2016, usRent2017, usRent2018, usRent2019],
@@ -62,6 +70,7 @@ function createPlots(countyName) {
             type: 'scatter'
         };
 
+        // Create trace for Oregon rental data
         let trace3 = {
             x: [2015, 2016, 2017, 2018, 2019],
             y: [orRent2015, orRent2016, orRent2017, orRent2018, orRent2019],
@@ -69,15 +78,17 @@ function createPlots(countyName) {
             type: 'scatter'
         };
 
-
+        // Pull in all three traces 
         let data1 = [trace1, trace2, trace3];
 
+        // Add labels to graph
         let layout = {
             title: countyName,
             xaxis: { title: "Year", tickformat: '.0f', dtick: 1 },
             yaxis: { title: "Average Rent ($)" }
         }
 
+        // Create graph
         Plotly.newPlot('plot', data1, layout);
 
 
